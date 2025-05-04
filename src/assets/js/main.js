91043587405f8960c1a0f91c84fd243ebda7ff80
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2025-05-04 11:08:54 -0500

    Adding posts

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,7 +6,7 @@ const stickyClasses = ['fixed', 'h-14'];
 const unstickyClasses = ['absolute', 'h-20'];
 const stickyClassesContainer = [
   'border-neutral-300/50',
-  'bg-white/80',
+  'bg-cream-light',
   'dark:border-neutral-600/40',
   'dark:bg-neutral-900/60',
   'backdrop-blur-2xl',
--- src/content/config.js
+++ src/content/config.js
@@ -1,14 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
-const postCollection = defineCollection({
+const postsCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    dateFormatted: z.string(),
+    category: z.string().optional(),
+    date: z.string(),
+    link: z.string().optional(),
   }),
 });
 
 export const collections = {
-  post: postCollection,
+  postsEs: postsCollection,
+  postsEn: postsCollection,
 };
